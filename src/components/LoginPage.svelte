3d3b112b887b7efc6d4e73dc03029ffe1cd54520
Utmite <rockimunoz84@gmail.com>
2023-12-24 01:33:08 -0300

    save docker compose and caddy

--- src/components/LoginPage.svelte
+++ src/components/LoginPage.svelte
@@ -54,7 +54,7 @@
         <input
             class="w-full p-4 text-sm bg-gray-50 focus:outline-none border border-gray-200 rounded text-gray-600"
             type="text"
-            placeholder="Email"
+            placeholder="Correo UC"
             bind:value={email}
         />
     </div>
